8c27f27f1fea285e51852e1ab388ad5c4277e53d
Ron Struempf <rstruem1@my.westga.edu>
2016-04-26 23:27:11 -0400

    Baby step 2 - Add Movie menu option

--- app/src/main/java/com/ronstruempf/myvideolibrary/view/MainActivity.java
+++ app/src/main/java/com/ronstruempf/myvideolibrary/view/MainActivity.java
@@ -9,6 +9,8 @@ import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -73,6 +75,57 @@ public class MainActivity extends AppCompatActivity {
             // TODO: Reload current list
         }
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        switch (item.getItemId()) {
+            case R.id.add_movie:
+                addNewMovie();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    /**
+     * Display an edit form for adding a new video
+     */
+    private void addNewMovie() {
+        // TODO: Run new movie activity
+    }
+
+    /**
+     * Display the details view for a video
+     *
+     * @param id Video to display
+     */
+    private void displayVideoDetails(int id) {
+        if (singlePage) {
+            Bundle arguments = new Bundle();
+            arguments.putString(VideoDetailFragment.ARG_VIDEO_ID, String.valueOf(id));
+            VideoDetailFragment fragment = new VideoDetailFragment();
+            fragment.setArguments(arguments);
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.video_detail_container, fragment)
+                    .commit();
+        } else {
+            Context context = this; // this.getApplicationContext();
+            Intent intent = new Intent(context, VideoDetailActivity.class);
+            intent.putExtra(VideoDetailFragment.ARG_VIDEO_ID, String.valueOf(id));
+            context.startActivity(intent);
+        }
+    }
+
 
     public static VideoLibraryController getController() {
         return _controller;
